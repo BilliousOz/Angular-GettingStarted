a12bd82c3de8ced177531d955685a52a303d29c3
Bill Stolk <bill@grandyzine.com>
2019-09-04 17:49:01 +1000

    Changes rel to "More on Components"

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { ProductListComponent } from './products/product-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,10 @@ import { AppComponent } from './app.component';
     AppComponent,
     ProductListComponent
   ],
-  imports: [ BrowserModule ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {  }

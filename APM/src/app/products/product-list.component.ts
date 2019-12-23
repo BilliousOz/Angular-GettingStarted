5ef25221cc4c29f73c6eda2ccfefecaa636cf302
Bill Stolk <bill@grandyzine.com>
2019-12-23 16:57:36 +1100

    Navigation and routing additional 1

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,6 +1,6 @@
 import { ProductService } from './product.service';
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './product';
+import { IProduct } from '../product/product';
 
 
 @Component({
--- APM/src/app/products/product.service.ts
+++ APM/src/app/products/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { IProduct } from './product';
+import { IProduct } from '../product/product';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, tap, throttleTime } from 'rxjs/operators';
